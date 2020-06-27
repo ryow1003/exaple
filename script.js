c30ee828ad398d0cd21aa551e0f3e4ad135926dc
Ryow <64392776+ryow1003@users.noreply.github.com>
2020-06-27 06:50:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,93 @@
 $(function(){
   $('.one').click(function(){
-    $('.fa-home').addClass('fa-spin');
+    $('.fa-home').addClass('fa-spin').delay(8000).queue(function(next){
+      $('.fa-home').removeClass('fa-spin');
+      next();
+    });
   });
   $('.two').click(function(){
-    $('.fa-laptop').addClass('fa-spin');
+    $('.fa-laptop').addClass('fa-spin').delay(8000).queue(function(next){
+      $('.fa-laptop').removeClass('fa-spin');
+      next();
+    });
   });
   $('.three').click(function(){
-    $('.fa-shopping-cart').addClass('fa-spin');
+    $('.fa-shopping-cart').addClass('fa-spin').delay(8000).queue(function(next){
+      $('.fa-shopping-cart').removeClass('fa-spin');
+      next();
+    });
   });
   $('.four').click(function(){
-    $('.fa-book-reader').addClass('fa-spin');
+    $('.fa-book-reader').addClass('fa-spin').delay(8000).queue(function(next){
+      $('.fa-book-reader').removeClass('fa-spin');
+      next();
+    });
   });
   $('.five').click(function(){
-    $('.fa-coffee').addClass('fa-spin');
+    $('.fa-coffee').addClass('fa-spin').delay(8000).queue(function(next){
+      $('.fa-coffee').removeClass('fa-spin');
+      next();
+    });
   });
   $('.six').click(function(){
-    $('.fa-hiking').addClass('fa-spin');
+    $('.fa-hiking').addClass('fa-spin').delay(8000).queue(function(next){
+      $('.fa-hiking').removeClass('fa-spin');
+      next();
+    });
   });
   $('.seven').click(function(){
-    $('.fa-instagram').addClass('fa-spin');
+    $('.fa-instagram').addClass('fa-spin').delay(8000).queue(function(next){
+      $('.fa-instagram').removeClass('fa-spin');
+      next();
+    });
   });
   $('.eight').click(function(){
-    $('.fa-twitter-square').addClass('fa-spin');
+    $('.fa-twitter-square').addClass('fa-spin').delay(8000).queue(function(next){
+      $('.fa-twitter-square').removeClass('fa-spin');
+      next();
+    });
   });
   $('.nine').click(function(){
-    $('.fa-youtube').addClass('fa-spin');
+    $('.fa-youtube').addClass('fa-spin').delay(8000).queue(function(next){
+      $('.fa-youtube').removeClass('fa-spin');
+      next();
+    });
   });
   $('.ten').click(function(){
-    $('.fa-newspaper').addClass('fa-spin');
+    $('.fa-newspaper').addClass('fa-spin').delay(8000).queue(function(next){
+      $('.fa-newspaper').removeClass('fa-spin');
+      next();
+    });
   });
   $('.eleven').click(function(){
-    $('.fa-exclamation-triangle').addClass('fa-spin');
+    $('.fa-exclamation-triangle').addClass('fa-spin').delay(8000).queue(function(next){
+      $('.fa-exclamation-triangle').removeClass('fa-spin');
+      next();
+    });
   });
   $('.twelve').click(function(){
-    $('.fa-envelop').addClass('fa-spin');
+    $('.fa-envelop').addClass('fa-spin').delay(8000).queue(function(next){
+      $('.fa-envelop').removeClass('fa-spin');
+      next();
+    });
   });
+  /*
+  $(function(){
+    ScrollReveal().reveal('.icon',{
+      duration:1000,
+      viewFactor:0.2,
+      reset:true,
+      opacity:0,
+      origin:'left'
+    });
+  });
+  $(function(){
+    ScrollReveal().reveal('.text',{
+      duration:1000,
+      viewFactor:0.2,
+      reset:true,
+      opacity:0,
+      origin:'left'
+    });
+  });*/
 });
